f96e6502868149ca71c728776b921af74bbc37a9
IsaacBosomtwe <asareike29@gmail.com>
2026-05-04 04:34:00 +0100

    Add all missing file contents

--- models/Crypto.js
+++ models/Crypto.js
@@ -1,1 +1,11 @@
-const mongoose = require('mongoose'); const cryptoSchema = new mongoose.Schema({ name: { type: String, required: true }, symbol: { type: String, required: true, uppercase: true }, price: { type: Number, required: true }, image: { type: String, default: '' }, change24h: { type: Number, default: 0 } }, { timestamps: true }); module.exports = mongoose.model('Crypto', cryptoSchema);
+const mongoose = require('mongoose');
+
+const cryptoSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  symbol: { type: String, required: true, uppercase: true },
+  price: { type: Number, required: true },
+  image: { type: String, default: '' },
+  change24h: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Crypto', cryptoSchema);
